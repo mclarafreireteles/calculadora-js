e52bbcd136697e127ba3f5960633ff0997f92843
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2024-10-03 19:34:11 -0300

    update script.js

--- script.js
+++ script.js
@@ -36,6 +36,15 @@ function criaCalculadora() {
 
         inicia() {
             this.cliqueBotoes()
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            })
         },
 
         cliqueBotoes() {
